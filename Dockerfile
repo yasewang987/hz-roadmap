9fff24526da3ac22de4164084432a39df2f59cbd
hzgod <hzgod@hzgoddeMBP.lan>
2021-02-10 22:07:54 +0800

    add roadmap

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,16 @@
 FROM nginx as base
 WORKDIR /app
-CMD ["nginx","-g", "daemon off;"]
+RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+EXPOSE 80
+
+FROM node:12.18-alpine as build
+WORKDIR /app
+COPY . .
+RUN yarn && yarn build && ls
+
+FROM base as final
+WORKDIR /app
+COPY --from=build /app/roadmap.conf /etc/nginx/conf.d
+COPY --from=build /app/cert /etc/nginx/conf.d
+COPY --from=build /app/dist /usr/share/nginx/html
+CMD [ "nginx", "-g", "daemon off;" ]
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld />
 </template>
 
 <script lang="ts">
@@ -16,12 +15,4 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
